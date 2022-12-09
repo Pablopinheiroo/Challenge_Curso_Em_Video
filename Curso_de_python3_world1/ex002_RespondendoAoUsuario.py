422fc2f0a5f7f27b1ebfb343c28b4757c93e2525
Pablo Pinheiro Ribeiro <87079515+Pablopinheiroo@users.noreply.github.com>
2022-12-09 18:00:03 -0300

    uploud arquivo python

---
+++ Curso_de_python3_world1/ex002_RespondendoAoUsuario.py
@@ -0,0 +1,5 @@
+""" Desafio 002
+Crie um programa que pergunte o seu nome e mostre na Tela: É um prazer te conhecer, nome! """
+
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer {}!'.format(nome))
