1ce49eab96ed345fc862102caf8ff6a9bbb1e0f4
Pablo Pinheiro Ribeiro <87079515+Pablopinheiroo@users.noreply.github.com>
2022-12-09 19:24:04 -0300

    adcionando ex 003

---
+++ Curso_de_python3_world1/ex003_SomandoDoisNumeros.py
@@ -0,0 +1,7 @@
+""" Desafio 003
+Crie um script python que leia dois números e tente mostrar a soma entre eles"""
+
+n1 = int(input('Informe o primeiro Número: '))
+n2 = int(input('Informe o Segundo Número: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
