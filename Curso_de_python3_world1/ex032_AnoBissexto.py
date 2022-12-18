65d89a9c67ba692aa7fbb57a9a3c1fbb1046a6f2
Pablo Pinheiro Ribeiro <87079515+Pablopinheiroo@users.noreply.github.com>
2022-12-18 11:18:41 -0300

    Upload ex032

--- Curso_de_python3_world1/ex032_AnoBissexto.py
+++ Curso_de_python3_world1/ex032_AnoBissexto.py
@@ -1,12 +1,16 @@
 """ Desafio 032
 Faça um programa que leia um ano qualquer e mostre se ele é bissexto."""
+
 from datetime import date
-msg = '   Separando o Primeiro e o Último Nome   '
+
+msg = '   Descubra se o Ano é ou Não um Ano Bissexto   '
 m = len(msg)
 print('-' * m)
 print('{}'.format(msg))
 print('-' * m)
+
 ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+
 if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 or 400 == 0:
