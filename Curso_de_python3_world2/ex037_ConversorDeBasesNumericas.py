c6b99dfa6e6eb163d10732bcbb9dfba6b21c0568
Pablo Pinheiro Ribeiro <87079515+Pablopinheiroo@users.noreply.github.com>
2022-12-20 20:22:36 -0300

    Upload ex037

--- Curso_de_python3_world2/ex037_ConversorDeBasesNumericas.py
+++ Curso_de_python3_world2/ex037_ConversorDeBasesNumericas.py
@@ -2,7 +2,7 @@
 Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher
 qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal."""
 
-msg = '        Aprovando ou Não o Empréstimo Bancário        '
+msg = '        Conversões de Bases Numéricas        '
 m = len(msg)
 print('\033[32m' + ('-' * m))
 print('{}'.format(msg))
